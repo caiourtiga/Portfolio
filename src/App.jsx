6595f299a89a2262976babc085a26d387fca7e3c
caiourtiga <caiourtiga@hotmail.com>
2023-03-29 23:32:09 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'
-import './components/Navbar'
-
+import Navbar from './components/Navbar';
   
+function App () {
+  return(
+    <Navbar/>
+  )
+}
 
 
-  export default App
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-<nav class="navbar navbar-dark bg-dark">
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,5 +24,7 @@
     </div>
   </div>
 </nav>
+  )
+}
 
 export default Navbar
