fdcae9b1f912cba0e979ed43492d19f68b7bf5d8
caiourtiga <caiourtiga@hotmail.com>
2023-04-16 13:21:14 -0300

    asga

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,40 +3,62 @@ import React, { Component } from 'react';
 const Body = () => {
     return (
       <body>
-       <div className="container d-flex justify-content-center intro">
-        <div className="row">
-          <div className="col-lg-6 d-flex align-items-center">
-           <h1>Seja bem vindo, sou Programador Web e será um prazer atendê-lo.</h1>
+        <div>
+          <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active" data-bs-interval="5000">
+                <img src="/assets/carou/3441_Puerto_mediterraneo.jpg" class="img-fluid d-block w-100 caimg" alt="..."></img>
+                <div class="carousel-caption d-none d-md-block">
+                  <p>Seja bem vindo, sou Programador Web e será um prazer atendê-lo.</p>
+                </div>
+              </div>
+              <div className="carousel-item" data-bs-interval="5000">
+                <img src="/assets/carou/DortorDordrecht.jpg" class="img-fluid d-block w-100 caimg" alt="..."></img>
+                <div class="carousel-caption d-none d-md-block">
+                  <p>Seja bem vindo, sou Programador Web e será um prazer atendê-lo.</p>
+                </div>
+              </div>
+              <div className="carousel-item" data-bs-interval="5000">
+                <img src="https://i.ytimg.com/vi/b2gaTEMyaa4/sddefault.jpg" class="img-fluid d-block w-100 caimg" alt="..."></img>
+                <div class="carousel-caption d-none d-md-block">
+                  <p>Seja bem vindo, sou Programador Web e será um prazer atendê-lo.</p>
+                </div>
+              </div>
             </div>
-            <div className="col-lg-6 d-flex align-items-center">
-             <img src="/assets/banana.png" class="rounded float-end cta-img" alt="Banana"></img>
-            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
         </div>
         <br/>
         <div>
-        <h2 className="text-center" id="sobre"> Sobre </h2>
+        <h5 className="text-center" id="sobre"> Sobre </h5>
           <div className="container skills">
-          <div className="row row-cols-auto g-2">
+          <div className="row row-cols-auto g-5">
             <div className="col-ms-auto">
-             <img src="/assets/html.png" alt="" className="img-fluid" style={{ width: '200px', height: '200px' }} />
+             <img src="/assets/tech/html.png" alt="" className="img-fluid timg"/>
             </div>
             <div className="col">
-              <img src="/assets/css.png" alt="" className="img-fluid" style={{ width: '200px', height: '200px' }} />
+              <img src="/assets/tech/css.png" alt="" className="img-fluid timg"/>
             </div>
             <div className="col">
-              <img src="/assets/js.png" alt="" className="img-fluid" style={{ width: '200px', height: '200px' }} />
+              <img src="/assets/tech/js.png" alt="" className="img-fluid timg"/>
             </div>
             <div className="col">
-              <img src="/assets/react.png" alt="" className="img-fluid" style={{ width: '200px', height: '200px' }} />
+              <img src="/assets/tech/react.png" alt="" className="img-fluid timg"/>
             </div>
           </div>
         </div>
         </div>
         <br/>
-        <h2 className="text-center" id="redes"> Redes </h2>
+        <h5 className="text-center" id="redes"> Redes </h5>
         <div className="container socials text-center">
-          <div className="row g-2">
+          <div className="row g-5">
             <div className="col-md-auto">
               <a href="https://github.com/caiourtiga/" target="_blank" rel="noopener noreferrer">
                 <img src="/assets/github.png" alt="Github" className="img-fluid img-thumbnail simg" />
@@ -48,7 +70,7 @@ const Body = () => {
               </a>
             </div>
             <div className="col-md-auto">
-              <a href="https://discord.com/users/231591118498824203" target="_blank" rel="noopener noreferrer">
+              <a href="https://discord.com/users/531591118498854503" target="_blank" rel="noopener noreferrer">
                 <img src="/assets/discord.png" alt="Discord" className="img-fluid img-thumbnail simg" />
               </a>
             </div>
@@ -57,25 +79,24 @@ const Body = () => {
         <br/>
         <div className="p-3 was-validated">
           <form action="https://formsubmit.co/caiourtiga@hotmail.com" method="POST">
-          <div class="mb-3">
-            <label for="InputEmail1" class="form-label"><h3>Email address</h3></label>
-            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" required></input>
-            <div id="emailHelp" class="form-text">fulano@hotmail.com</div>
-          </div>
-          <div class="mb-3">
-            <label for="InputPhone" class="form-label"><h3>Telefone</h3></label>
-            <input type="number" min="8" class="form-control" id="InputWhats" aria-describedby="WhatsHelp" required></input>
-            <div id="WhatsHelp" class="form-text">Somente Whatsapp</div>
-          </div>
-          <div class="mb-3">
-            <label for="Textarea1" class="form-label"><h3>Mensagem</h3></label>
-            <textarea class="form-control" id="Textarea1" rows="3" required></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+            <div className="mb-3">
+              <label for="InputEmail1" className="form-label"><h3>Email address</h3></label>
+              <input type="email" className="form-control" id="InputEmail1" aria-describedby="emailHelp" required></input>
+              <div id="emailHelp" className="form-text">fulano@hotmail.com</div>
+            </div>
+            <div className="mb-3">
+              <label for="InputPhone" className="form-label"><h3>Telefone</h3></label>
+              <input type="number" min="8" className="form-control" id="InputWhats" aria-describedby="WhatsHelp" required></input>
+              <div id="WhatsHelp" className="form-text">Somente Whatsapp</div>
+            </div>
+            <div className="mb-3">
+              <label for="Textarea1" className="form-label"><h3>Mensagem</h3></label>
+              <textarea className="form-control" id="Textarea1" rows="3" required></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Enviar</button>
+          </form>
         </div>
-      </body>
-           
+      </body>     
     )
   }
   
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,27 @@
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark">
-  <div class="container">
-    <a class="navbar-brand" href="">Caio Urtiga</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+  <div className="container">
+    <a className="navbar-brand" href="">Caio Urtiga</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Início</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="">Início</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#sobre">Sobre</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#sobre">Sobre</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#redes">Redes</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#redes">Redes</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://github.com/caiourtiga/zport">Repositório</a>
+        <li className="nav-item">
+          <a className="nav-link" href="https://github.com/caiourtiga/zport">Repositório</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contato</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contato</a>
         </li>
       </ul>
     </div>
