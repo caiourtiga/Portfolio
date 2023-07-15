20d86949a0e9c4b62956f660bc3609860cb7acb7
caiourtiga <caiourtiga@hotmail.com>
2023-07-14 21:25:57 -0300

    adada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
+<nav className="navbar navbar-expand-lg navbar-dark">
   <div className="container">
     <a className="navbar-brand" href="">Caio Urtiga</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
